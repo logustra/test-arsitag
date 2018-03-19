5b5ead0d326d4bf40fbdbf0ed487a9d3443f79b1
logustra <logustra@gmail.com>
2018-03-19 13:00:23 +0700

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Profile from './scenes/Profile/Profile';
+
+class App extends Component {
+    render() {
+        return (
+            <Profile />
+        )
+    }
+}
+
+export default App;
+
---
+++ src/scenes/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Aux from './../../hoc/Aux/Aux';
+
+class Profile extends Component {
+    render() {
+        return (
+            <Aux>
+                <p>Bismillah</p>
+            </Aux>
+        )
+    }
+}
+
+export default Profile;
